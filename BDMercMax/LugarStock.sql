cc6eedf59f470c9f9bfa46c0f0621a6ebe26f6f8
Esteban Garizabalo <estebangarizabalo@hotmail.es>
2021-11-04 21:57:18 -0500

    BDMercamax
    se sube base de datos mercamax

---
+++ BDMercMax/LugarStock.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[LugarStock]
+(
+	[id_lugar] INT NOT NULL PRIMARY KEY, 
+    [barcode_producto] INT NOT NULL UNiQUE, 
+    [cantidad_bodega] INT NOT NULL, 
+    [seccion_bodega] NVARCHAR(MAX) NOT NULL, 
+    [seccion_gondola] NVARCHAR(MAX) NOT NULL
+)
